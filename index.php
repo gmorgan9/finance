ca106aba5bbb1c6149926840e288e1464a2d2d19
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-06 21:14:10 -0700

    m

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-test from php file
+<?php 
+include("path.php");
+// include(ROOT_PATH . "/app/controllers/topics.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
+  
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="/assets/images/fav.png">
+
+  <!-- Custome Styles -->
+  <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
+
+  <title>Documentation</title>
+</head>
+<body>
+
+<?php include(ROOT_PATH . "/app/includes/Header.php")  
+// include(ROOT_PATH . "/app/includes/main-heading.php") 
+// include(ROOT_PATH . "/app/includes/messages.php"); 
+?>
+
+
+
+  <!-- Page Wrapper -->
+  <div class="page-wrapper">
+
+    
+
+  </div>
+  <!-- // Page Wrapper -->
+</body>
+</html>
