3adc38df8b01588cdcfa8a0db3142ba4fcc08471
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-07 20:56:24 -0700

    m

--- database/connect.php
+++ database/connect.php
@@ -1,12 +1,16 @@
 <?php
-
-$host = 'localhost';
-$user = 'gmorg';
-$pass = 'gmorgpass';
-$db_name = 'users';
-
-$conn = new MySQLi($host, $user, $pass, $db_name);
-
-if ($conn->connect_error) {
-    die('Database connection error: ' . $conn->connect_error);
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'gmorg');
+define('DB_PASSWORD', 'gmorgpass');
+define('DB_NAME', 'finance');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+?>
