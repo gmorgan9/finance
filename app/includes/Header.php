15cdad573683f8f56018f5a400dd458b9bab3b0b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-07 20:29:47 -0700

    m

--- app/includes/Header.php
+++ app/includes/Header.php
@@ -4,8 +4,6 @@
     </a>
     <i class="fa fa-bars menu-toggle"></i>
     <ul class="nav">
-      
-            <li>
             <a href="/">
             <i class="fa fa-globe"></i>
             Public</a>
@@ -17,14 +15,8 @@
                     <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
                 </a>
                 <ul>
-                <?php if(!isLoggedIn()){?>
                     <li><a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout">Logout</a></li>
-                    <?php }else{ ?>
-                    <li><a href="<?php echo BASE_URL . '/login.php'; ?>" class="logout">Login</a></li>
-                    <!-- <li><a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout">Register</a></li> -->
-                    <?php } ?>
                 </ul>
             </li>
-     
     </ul>
 </header>
