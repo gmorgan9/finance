94a38b8849f7aa00e2b5b0be516b0317ef419df8
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-07 20:35:38 -0700

    m

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
-<?php include('path.php'); ?>
-<?php include(ROOT_PATH . "/app/controllers/users.php"); 
+<?php include("path.php"); ?>
+<?php include(ROOT_PATH . "/app/controllers/users.php");
 //guestsOnly();
 ?>
 <!DOCTYPE html>
@@ -10,45 +10,54 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="/assets/images/fav.png">
+
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-  <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="/assets/images/fav.png">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Candal|Lora" rel="stylesheet">
 
   <!-- Custom Styling -->
-  <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="assets/css/style.css">
 
-  <title>Login</title>
+  <title>Register</title>
 </head>
 
 <body>
-
+  
+  
 <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
   <div class="auth-content">
 
-    <form action="login.php" method="post">
-      <h2 class="form-title">Login</h2>
+    <form action="register.php" method="post">
+      <h2 class="form-title">Register</h2>
 
       <?php include(ROOT_PATH . "/app/helpers/formErrors.php"); ?>
 
       <div>
         <label>Username</label>
-        <input type="text" name="username" value="<?php echo $username; ?>" class="text-input">
+        <input type="text" name="username" value="<?php echo $username; ?>" class="text-input" >
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" name="email"  value="<?php echo $email; ?>" class="text-input">
       </div>
       <div>
         <label>Password</label>
-        <input type="password" name="password" value="<?php echo $password; ?>" class="text-input">
+        <input type="password" name="password"  value="<?php echo $password; ?>" class="text-input">
       </div>
       <div>
-        <button type="submit" name="login-btn" class="btn btn-big">Login</button>
+        <label>Password Confirmation</label>
+        <input type="password" name="passwordConf"  value="<?php echo $passwordConf; ?>" class="text-input">
       </div>
-      <p>Or <a href="<?php echo BASE_URL . '/register.php' ?>">Sign Up</a></p>
+      <div>
+        <button type="submit" name="register-btn" class="btn btn-big">Register</button>
+      </div>
+      <p>Or <a href="<?php echo BASE_URL . '/login.php' ?>">Sign In</a></p>
     </form>
 
   </div>
