e38bcaa868c44a4b743a83ba0ee66b0706594efb
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 23:12:18 -0700

    m

--- edituser.php
+++ edituser.php
@@ -1,112 +1,54 @@
+<?php include('functions.php'); ?>
 
+<?php 
+if (!isAdmin()) {
+	header('location: /');
+}
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+<head>
+	<title>Admin - Create user</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
+</head>
+<body>
+	<div class="log-header">
+		<h2>Admin - create user</h2>
+	</div>
+	
+	<form method="post" action="create_user.php">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<?php echo display_error(); ?>
 
-        <!-- Font Awesome -->
-        <link rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-            integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-            crossorigin="anonymous">
-
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Candal|Lora"
-            rel="stylesheet">
-
-        <!-- Custom Styling -->
-        <link rel="stylesheet" href="../../assets/css/style.css">
-
-        <!-- Admin Styling -->
-        <link rel="stylesheet" href="../../assets/css/admin.css">
-
-        <title>Admin Section - Edit User</title>
-    </head>
-
-    <body>
-        
-    <?php include(ROOT_PATH . "/app/includes/adminHeader.php"); ?>
-
-        <!-- Admin Page Wrapper -->
-        <div class="admin-wrapper">
-
-        <?php include(ROOT_PATH . "/app/includes/adminSidebar.php"); ?>
-
-
-            <!-- Admin Content -->
-            <div class="admin-content">
-                <div class="button-group">
-                    <a href="create.php" class="btn btn-big">Add User</a>
-                    <a href="index.php" class="btn btn-big">Manage Users</a>
-                </div>
-
-
-                <div class="content">
-
-                    <h2 class="page-title">Edit User</h2>
-
-                    <?php include(ROOT_PATH . "/app/helpers/formErrors.php"); ?>
-
-                    <form action="edit.php" method="post">
-                        <input type="hidden" name="id" value="<?php echo $id; ?>" >
-                        <div>
-                            <label>Username</label>
-                            <input type="text" name="username" value="<?php echo $username; ?>" class="text-input">
-                        </div>
-                        <div>
-                            <label>Email</label>
-                            <input type="email" name="email" value="<?php echo $email; ?>" class="text-input">
-                        </div>
-                        <div>
-                            <label>Password</label>
-                            <input type="password" name="password" value="<?php echo $password; ?>" class="text-input">
-                        </div>
-                        <div>
-                            <label>Password Confirmation</label>
-                            <input type="password" name="passwordConf" value="<?php echo $passwordConf; ?>" class="text-input">
-                        </div>
-                        <div>
-                            <?php if (isset($admin) && $admin == 1): ?>
-                                <label>
-                                    <input type="checkbox" name="admin" checked>
-                                    Admin
-                                </label>
-                            <?php else: ?>
-                                <label>
-                                    <input type="checkbox" name="admin">
-                                    Admin
-                                </label>
-                            <?php endif; ?>
-                            
-                        </div>
-
-                        <div>
-                            <button type="submit" name="update-user" class="btn btn-big">Update User</button>
-                        </div>
-                    </form>
-
-                </div>
-
-            </div>
-            <!-- // Admin Content -->
-
-        </div>
-        <!-- // Page Wrapper -->
-
-
-
-        <!-- JQuery -->
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <!-- Ckeditor -->
-        <script
-            src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
-        <!-- Custom Script -->
-        <script src="../../assets/js/scripts.js"></script>
-
-    </body>
-
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" value="<?php echo $username; ?>">
+		</div>
+		<div class="input-group">
+			<label>Email</label>
+			<input type="email" name="email" value="<?php echo $email; ?>">
+		</div>
+		<div class="input-group">
+			<label>User type</label>
+			<select name="user_type" id="user_type" >
+				<option value=""></option>
+				<option value="admin">Admin</option>
+				<option value="user">User</option>
+			</select>
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password_1">
+		</div>
+		<div class="input-group">
+			<label>Confirm password</label>
+			<input type="password" name="password_2">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="log-btn" name="register_btn">Create User</button>
+			<a href="profileinfo.php" name="btn" class="log-btn">Back</a>
+		</div>
+	</form>
+</body>
 </html>
