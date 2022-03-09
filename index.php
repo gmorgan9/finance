3fecbb2fe1f3952fc21dcf74f0512b8ca2ccb50b
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 23:37:45 -0700

    m

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ if (!isLoggedIN()) {
   <div class="dash-cards">
   <div class="card" id="currexp" style="width: 18rem;">
   <div class="card-body">
+    <i class="fa-solid fa-calendar-days"></i>
     <h5 class="card-title">Current Expenses</h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
