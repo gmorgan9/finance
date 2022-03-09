7cbb3fc9536cf6b63823c8fc75adcad3250aba51
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 21:35:26 -0700

    m

--- header.php
+++ header.php
@@ -3,12 +3,7 @@
     <h1 class="logo-text"><span>Garrett</span>Inspires</h1>
   </a>
   <i class="fa fa-bars menu-toggle"></i>
-    <ul class="nav">
-      <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="transactions.php">Transactions</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="expenses.php">Expenses</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="income.php">Income</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="budget.php">Budget</a></li>
+    
     </div>
     <hr style="width:50%">
     
@@ -28,8 +23,13 @@
             <?php echo $_SESSION['username']; ?>
             <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
           </a>
-          <ul>
           <div class="nav-list-log">
+          <ul class="nav">
+      <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
+      <li class="nav-list-item"><a class="nav-list-link" href="transactions.php">Transactions</a></li>
+      <li class="nav-list-item"><a class="nav-list-link" href="expenses.php">Expenses</a></li>
+      <li class="nav-list-item"><a class="nav-list-link" href="income.php">Income</a></li>
+      <li class="nav-list-item"><a class="nav-list-link" href="budget.php">Budget</a></li>
       <?php if(isLoggedIn()){?>
         <div id="profilelogout">
           <li class="nav-list-item"><a id="profile" href="profileinfo.php">Profile</a></li>
