bba9083c7c78d04a264af8391353db248255ec99
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 21:39:13 -0700

    m

--- header.php
+++ header.php
@@ -17,18 +17,17 @@
     </a>
     <i class="fa fa-bars menu-toggle"></i>
     <ul class="nav">
-        <li>
-          <a href="#">
-            <i class="fa fa-user"></i>
-            <?php echo $_SESSION['user']['username']; ?>
-            <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
-          </a>
-          <ul>
       <li><a href="index.php">Home</a></li>
       <li><a href="transactions.php">Transactions</a></li>
       <li><a href="expenses.php">Expenses</a></li>
       <li><a href="income.php">Income</a></li>
       <li><a href="budget.php">Budget</a></li>
+      <li><a href="#">
+          <i class="fa fa-user"></i>
+          <?php echo $_SESSION['user']['username']; ?>
+          <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+        </a>
+        <ul>
       <?php if(isLoggedIn()){?>
         <div id="profilelogout">
           <li><a href="profileinfo.php">Profile</a></li>
