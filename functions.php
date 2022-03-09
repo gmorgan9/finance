887a445e314b968d742706cd74e2061308027607
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 21:07:58 -0700

    m

--- functions.php
+++ functions.php
@@ -146,7 +146,7 @@ function login(){
 				
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
-				header('location: index.php');		  
+				header('location: /');		  
 			}else{
 				$_SESSION['user'] = $logged_in_user;
 				$_SESSION['success']  = "You are now logged in";
