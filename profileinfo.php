2e81ce2a28e0d36dbe648e108e311c0e15d97dbc
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 10:26:30 -0700

    m

--- profileinfo.php
+++ profileinfo.php
@@ -12,9 +12,9 @@
 	<meta name="viewport" content="width=device-width">
 	
 	<!-- Custom Styles -->
-    <link rel="stylesheet" href="css/styles.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="assets/css/styles.css?v=<?php echo time(); ?>">
     <!-- Admin Styles -->
-    <link rel="stylesheet" href="css/admin.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="assets/css/admin.css?v=<?php echo time(); ?>">
 
 	<!-- Font Awesome -->
     <link rel="stylesheet"
