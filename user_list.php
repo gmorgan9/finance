80e351d5f54501851232db663a4ab9c0d6c581ef
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 22:37:01 -0700

    m

--- user_list.php
+++ user_list.php
@@ -70,6 +70,16 @@ if(mysqli_num_rows($result) > 0) {
 }
 ?>
 
+<?php foreach ($users as $key => $user): ?>
+    <tr>
+        <td><?php echo $key + 1; ?></td>
+        <td><?php echo $user['username']; ?></td>
+        <td><?php echo $user['email']; ?></td>
+        <td><a href="edit.php?id=<?php echo $user['id']; ?>" class="edit">edit</a></td>
+        <td><a href="index.php?delete_id=<?php echo $user['id']; ?>" class="delete">delete</a></td>
+    </tr>
+<?php endforeach; ?>
+
 <td><?php echo $id; ?></td>
 <td><?php echo $username; ?></td>
 <td><?php echo $email; ?></td>
