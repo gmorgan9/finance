555255ce0d0ab6d54c5a3c01018c160e12bb2710
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 09:18:44 -0700

    m

--- edituser.php
+++ edituser.php
@@ -86,11 +86,11 @@
 
 
         <!-- JQuery -->
-        <script
-            src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
         <!-- Ckeditor -->
-        <script
-            src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script>
+        <!-- <script
+            src="https://cdn.ckeditor.com/ckeditor5/12.2.0/classic/ckeditor.js"></script> -->
         <!-- Custom Script -->
         <!-- <script src="../../assets/js/scripts.js"></script> -->
 
