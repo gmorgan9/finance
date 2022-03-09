b7630008a507307a1a337c82235100d2d5c8bc68
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 21:30:08 -0700

    m

--- header.php
+++ header.php
@@ -1,5 +1,9 @@
-<div class="nav-header">
-    <div id="nav-list">
+<!-- <header>
+  <a href="/" class="logo">
+    <h1 class="logo-text"><span>Garrett</span>Inspires</h1>
+  </a>
+  <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
       <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
       <li class="nav-list-item"><a class="nav-list-link" href="transactions.php">Transactions</a></li>
       <li class="nav-list-item"><a class="nav-list-link" href="expenses.php">Expenses</a></li>
@@ -20,4 +24,37 @@
         </div>
 		  <?php } ?>
     </div>
-  </div>
+</header> -->
+
+
+
+  <header>
+    <a href="<?php echo BASE_URL . '/' ?>" class="logo">
+      <h1 class="logo-text"><span>Garrett</span>Inspires</h1>
+    </a>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+      <!-- <li><a href="<?php 
+      // echo 
+      // BASE_URL . '/' ?>">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="<?php 
+      // echo BASE_URL . '/' ?>">Documentation</a></li> -->
+
+      <?php if (isset($_SESSION['id'])): ?>
+        <li>
+          <a href="#">
+            <i class="fa fa-user"></i>
+            <?php echo $_SESSION['username']; ?>
+            <i class="fa fa-chevron-down" style="font-size: .8em;"></i>
+          </a>
+          <ul>
+          <?php if(isLoggedIn()){?>
+              <li><a href="<?php echo BASE_URL . '/admin/dashboard.php' ?>">Dashboard</a></li>
+            <?php }else {?>
+            <li><a href="<?php echo BASE_URL . '/logout.php' ?>" class="logout">Logout</a></li>
+          </ul>
+        </li>
+        <?php }?>
+    </ul>
+</header>
