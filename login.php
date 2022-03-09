04285c53cdb2203d23ed480d29a34aa5cf092c26
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 21:43:33 -0700

    m

--- login.php
+++ login.php
@@ -12,6 +12,17 @@ if (isLoggedIN()) {
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
 	<link rel="icon" type="image/x-icon" href="fav.png?v=<?php echo time(); ?>">
+
+	<!-- Font Awesome -->
+    <link rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+    crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Candal|Lora"
+    rel="stylesheet">
+    
 </head>
 <body>
 <?php include("header.php") ?>
--- profileinfo.php
+++ profileinfo.php
@@ -12,6 +12,17 @@
 	<title>Profile Info</title>
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
+
+	<!-- Font Awesome -->
+    <link rel="stylesheet"
+    href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+    crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Candal|Lora"
+    rel="stylesheet">
+    
 </head>
 <body>
 	<div class="log-header">
