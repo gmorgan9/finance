e1d1fb309a3ecf1d204399e21a243ae43387b2b0
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 09:11:42 -0700

    m

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ if (!isLoggedIN()) {
 </div>
 <div class="card" id="totinc" style="width: 18rem;">
   <div class="card-body">
-  <img src="balance.png" alt="" style="height: 45px; width: 35px;">
+  <img src="balance.png" alt="" style="height: 45px; width: 45px;">
     <h5 class="card-title">Total Income</h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
