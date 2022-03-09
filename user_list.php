192b8e70b4103f35153c22ba621b0abbad538f9c
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 22:15:55 -0700

    m

--- user_list.php
+++ user_list.php
@@ -1,6 +1,5 @@
 <?php 
 	include('functions.php');
-    include('usercontroller.php')
     if (!isLoggedIn()) {
         $_SESSION['msg'] = "You must log in first";
         header('location: login.php');
