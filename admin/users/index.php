899550b6182a566994d2f4a783802e0da336ecad
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-09 10:48:26 -0700

    m

--- admin/users/index.php
+++ admin/users/index.php
@@ -1,5 +1,5 @@
 <?php 
-	include("app/database/functions.php");
+	include("/app/database/functions.php");
     if (!isLoggedIn()) {
         $_SESSION['msg'] = "You must log in first";
         header('location: login.php');
