c9833dc932f22800d3ba4103993c7032c068f6f3
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 23:11:02 -0700

    m

--- user_list.php
+++ user_list.php
@@ -69,7 +69,7 @@ echo "<td>" . $row['id'] . "</td>";
 echo "<td>" . $row['username'] . "</td>";
 echo "<td>" . $row['user_type'] . "</td>";
 ?>
-<td><a href="edit.php?id=<?php echo $row['id']; ?>" class="edit">edit</a></td>
+<td><a href="edituser.php?id=<?php echo $row['id']; ?>" class="edit">edit</a></td>
 <td><a href="user_list.php?delete_id=<?php echo $row['id']; ?>" class="delete">delete</a></td>
 <?php
 echo "</tr>";
