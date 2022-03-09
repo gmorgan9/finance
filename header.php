4cdd490aae6aad72d51ac081b5bb05892b0365a0
Garrett Morgan <gar.mor.pro@outlook.com>
2022-03-08 21:32:14 -0700

    m

--- header.php
+++ header.php
@@ -1,30 +1,4 @@
-<!-- <header>
-  <a href="/" class="logo">
-    <h1 class="logo-text"><span>Garrett</span>Inspires</h1>
-  </a>
-  <i class="fa fa-bars menu-toggle"></i>
-    <ul class="nav">
-      <li class="nav-list-item"><a class="nav-list-link" href="index.php">Home</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="transactions.php">Transactions</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="expenses.php">Expenses</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="income.php">Income</a></li>
-      <li class="nav-list-item"><a class="nav-list-link" href="budget.php">Budget</a></li>
-    </div>
-    <hr style="width:50%">
-    <div class="nav-list-log">
-      <?php if(isLoggedIn()){?>
-        <div id="profilelogout">
-          <li class="nav-list-item"><a id="profile" href="profileinfo.php">Profile</a></li>
-          <li class="nav-list-item"><a id="logout" href="index.php?logout='1'">Logout</a></li>
-        </div>
-		  <?php }else{ ?>
-        <div id="loginreg">
-          <li class="nav-list-item"><a id="login" href="login.php">Login</a></li>
-          <li class="nav-list-item"><a id="register" href="register.php">Register</a></li>
-        </div>
-		  <?php } ?>
-    </div>
-</header> -->
+
 
 
 
